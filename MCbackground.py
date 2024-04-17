d1404f44d70ef4b605947014efb3f91df7335363
adilthedealer <a_salkimbayev@kbtu.kz>
2024-04-17 10:21:44 +0500

    версия кода (менять + добавлять бэкграунды, добавить механики смены бэкграндов при дохождении до координат = 0, равных screen_width, screen_height, добавлять механики передвижения ног)

--- MCbackground.py
+++ MCbackground.py
@@ -15,27 +15,43 @@ pygame.display.set_caption("тест")
 # Загрузка изображения фона
 background = pygame.image.load("images/background.jpg")
 
-class Student(pygame.sprite.Sprite):
+class Runner(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("images/MainCharacterRightSide.png")
+        self.image = pygame.image.load("images/standing_student.png")
         self.rect = self.image.get_rect()
         self.rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
         self.speed = 5
 
     def update(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
+
+        if keys[pygame.K_LEFT] and self.rect.left > 0:
             self.rect.x -= self.speed
-        if keys[pygame.K_RIGHT]:
+            self.image = pygame.image.load("images/running_student1.png")
+            if self.rect.left < 0:
+                self.rect.left = 0
+
+        if keys[pygame.K_RIGHT] and self.rect.right < SCREEN_WIDTH:
             self.rect.x += self.speed
-        if keys[pygame.K_UP]:
+            self.image = pygame.image.load("images/running_student1.png")
+            if self.rect.right > SCREEN_WIDTH:
+                self.rect.right = SCREEN_WIDTH
+
+        if keys[pygame.K_UP] and self.rect.top > 0:
             self.rect.y -= self.speed
-        if keys[pygame.K_DOWN]:
+            self.image = pygame.image.load("images/running_student2.png")
+            if self.rect.top < 0:
+                self.rect.top = 0
+
+        if keys[pygame.K_DOWN] and self.rect.bottom < SCREEN_HEIGHT:
             self.rect.y += self.speed
+            self.image = pygame.image.load("images/running_student2.png")
+            if self.rect.bottom > SCREEN_HEIGHT:
+                self.rect.bottom = SCREEN_HEIGHT
 
 all_sprites = pygame.sprite.Group()
-character = Student()
+character = Runner()
 all_sprites.add(character)
 
 running = True
