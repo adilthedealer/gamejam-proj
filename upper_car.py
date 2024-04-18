a18043c5344d84c58d4337b51b81a934c0cbd855
adilthedealer <a_salkimbayev@kbtu.kz>
2024-04-18 22:17:47 +0500

    изменение

--- upper_car.py
+++ upper_car.py
@@ -6,15 +6,19 @@ class Upper_car:
         self.speed = 8
         self.rect = self.image.get_rect().inflate(-50, -50)
         self.rect.topleft = (x, y)
-        self.stopped = False  # Добавляем атрибут, чтобы отслеживать, остановлена ли машина
+        self.stopped = False  # Added attribute to track if the car is stopped
         
     def move(self, traffic_light):
-        if traffic_light.current_light_index == 2:  # Если текущий цвет красный
-            self.stopped = True  # Останавливаем машину
+        if traffic_light.current_light_index == 2:  # If the current light is red
+            # Check if the car has reached the stop lines
+            if 95 <= self.rect.left <= 275:
+                self.stopped = True  # Stop the car
+            else:
+                self.stopped = False  # Otherwise, the car can move
         else:
-            self.stopped = False  # В противном случае машина может двигаться
+            self.stopped = False  # If the light is not red, the car can move
 
-        if not self.stopped:  # Если машина не остановлена
+        if not self.stopped:  # If the car is not stopped
             self.rect.x += self.speed
             if self.rect.left > 1600 or self.rect.right < 0:
                 self.speed = -self.speed
