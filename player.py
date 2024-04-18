5e14f6436c9256cfb0141bdb35ad1b9c4452a9b3
adilthedealer <a_salkimbayev@kbtu.kz>
2024-04-18 15:51:57 +0500

    бег назад работает

--- player.py
+++ player.py
@@ -3,40 +3,36 @@ import pygame as pg
 class Player:
     def __init__(self):
         img1 = pg.image.load("images/mc_run.png").convert_alpha()
-        fix1 = pg.transform.scale(img1, (35, 35))
+        self.fix1 = pg.transform.scale(img1, (35, 35))
+        
         img2 = pg.image.load("images/mc_run2.png").convert_alpha()
-        fix2 = pg.transform.scale(img2, (35, 35))
+        self.fix2 = pg.transform.scale(img2, (35, 35))
+        
         img = pg.image.load("images/mc_stand.png").convert_alpha()
-        fix = pg.transform.scale(img, (35, 35))
-        self.run_images = [
-            # pg.image.load("images/running_student1.png").convert_alpha(),
-            # pg.image.load("images/running_student2.png").convert_alpha(),
-            fix1,
-            fix,
-            fix2
-        ]
+        self.fix = pg.transform.scale(img, (35, 35))
+
+        img_back1 = pg.image.load("images/mcback/mc_runback_1.png").convert_alpha()
+        self.fixb1 = pg.transform.scale(img_back1, (35, 35))
+        
+        img_back2 = pg.image.load("images/mcback/mc_runback_2.png").convert_alpha()
+        self.fixb2 = pg.transform.scale(img_back2, (35, 35))
+        
+        img_back = pg.image.load("images/mcback/mc_look_back.png").convert_alpha()
+        self.fixb = pg.transform.scale(img_back, (35, 35))
+
+        self.run_images = [self.fix1, self.fix, self.fix2]
         self.current_run_image = 0
         self.image = self.run_images[self.current_run_image]
+
         self.rect = self.image.get_rect().inflate(-50, -50)
         self.rect.center = (1600, 1800)
 
-
-
     def move(self, vector):
-        # if vector[1] > 0:
-        #     self.run_images = [
-        #         pg.image.load("images/mcback/mc_runback_1.png").convert_alpha(),
-        #         pg.image.load("images/mcback/mc_runback_2.png").convert_alpha()
-        #     ]
-        #     self.current_run_image = 0
-        #     self.image = self.run_images[self.current_run_image]
-        # else:
-        # self.run_images = [
-        #     pg.image.load("images/running_student1.png").convert_alpha(),
-        #     pg.image.load("images/running_student2.png").convert_alpha()
-        # ]
-        # self.current_run_image = 0
-        # self.image = self.run_images[self.current_run_image]
+        if vector[1] <= 0:
+            self.run_images = [self.fix1, self.fix, self.fix2]
+        else:
+            self.run_images = [self.fixb1, self.fixb, self.fixb2]
+
         self.rect.x += vector[0]
         self.rect.y += vector[1]
 
