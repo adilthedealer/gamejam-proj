cf8cac0c48f170b323752c03a00f916796f8b60e
adilthedealer <a_salkimbayev@kbtu.kz>
2024-04-19 20:54:56 +0500

    фича

--- lower_car.py
+++ lower_car.py
@@ -1,18 +1,19 @@
 import pygame
+import random as rnd
 
 class Lower_car:
     def __init__(self, x, y, color):
         self.image = pygame.image.load("images/cars/" + color + "_car.png")
-        self.speed = 8
+        self.speed = rnd.randint(7, 14)
         self.rect = self.image.get_rect().inflate(-50, -50)
         self.rect.topleft = (x, y)
         self.stopped = False  # Added attribute to track if the car is stopped
         
     def move(self, traffic_light):
-        if traffic_light.current_light_index == 2:  # If the current light is red
-            self.stopped = True  # Stop the car
-        else:
-            self.stopped = False  # Otherwise, the car can move
+        # if traffic_light.current_light_index == 2:  # If the current light is red
+        #     self.stopped = True  # Stop the car
+        # else:
+        #     self.stopped = False  # Otherwise, the car can move
 
         if not self.stopped:  # If the car is not stopped
             self.rect.x += self.speed
